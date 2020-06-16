6816365cef613a07575b5124d33126d48680d7cf
Surjeet <gangwarsurjeet24@gmail.com>
2020-06-16 22:26:23 +0530

    Project Initialization code

---
+++ ArticleTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArticleTestApp
+//
+//  Created by Surjeet on 16/06/20.
+//  Copyright © 2020 Surjeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
